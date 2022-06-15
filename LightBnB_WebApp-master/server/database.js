039295d9f3cf5d526224bdecebf061c3fa42e9c1
Caitlin <cjcroteau@gmail.com>
2022-06-15 03:14:11 +0000

    refactor addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -85,6 +85,8 @@ const addUser =  function(user) {
   return pool
   .query(queryString, values)
   .then(result => {
+    console.log('add user result rows', result.rows);
+    console.log('add user result rows at 0', result.rows[0])
     return result.rows[0];
   })
   .catch(err => {
@@ -241,9 +243,54 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const values = [property.owner_id,
+  property.title,
+  property.description,
+  property.thumbnail_photo_url,
+  property.cover_photo_url,
+  property.cost_per_night,
+  property.street,
+  property.city,
+  property.province,
+  property.post_code,
+  property.country,
+  property.parking_spaces,
+  property.number_of_bathrooms,
+  property.number_of_bedrooms];
+
+  const queryString = `
+  INSERT into properties (owner_id,
+  title,
+  description,
+  thumbnail_photo_url,
+  cover_photo_url,
+  cost_per_night,
+  street,
+  city,
+  province,
+  post_code,
+  country,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+
+  return pool
+  .query(queryString, values)
+  .then(result => {
+    console.log('added prop', result.rows[0])
+    return result.rows[0];
+  })
+  .catch(err => {
+    console.log('err', err);
+    return null;
+  })
+
+  //OG CODE:
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
